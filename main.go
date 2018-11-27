0421097c511cdc4e767b50263ca35ba4ccd3a454
Alberto Ricart <alberto@synadia.com>
2018-11-26 18:11:45 -0600

    changed formatting of the build artifact so the updater can see it added code to do the initial check of the updater

--- main.go
+++ main.go
@@ -9,5 +9,22 @@ var version = "0.0.0.dev"
 
 func main() {
 	cmd.GetRootCmd().Version = version
+	cmd.SetUpdateRespository("nats-io/nsc")
+
+	u, err := cmd.NewSelfUpdate()
+	if err != nil {
+		// not fatal
+		cmd.GetRootCmd().Printf("error checking for updates: %v\n", err)
+	}
+	if u != nil {
+		m, err := u.Run()
+		if err != nil {
+			cmd.GetRootCmd().Printf("error checking for updates: %v\n", err)
+		}
+		if m != "" {
+			cmd.GetRootCmd().Printf("new version available %s - `nsc update` to update.\n", m)
+		}
+	}
+
 	cmd.Execute()
 }
