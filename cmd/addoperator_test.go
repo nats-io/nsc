de5d02b10f368e4e085c0bb685c26f1d08dd2e4b
Alberto Ricart <alberto@synadia.com>
2019-05-22 09:22:13 -0500

    - Added tests to add operator (#84)
    - Fixed add operator from an import
    - Removed code that generated creds file for an operator

---
+++ cmd/addoperator_test.go
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2018-2019 The NATS Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/nats-io/jwt"
+	"github.com/nats-io/nsc/cmd/store"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_AddOperator(t *testing.T) {
+	ts := NewEmptyStore(t)
+	t.Log(ts.Dir)
+
+	_, err := os.Lstat(filepath.Join(ts.Dir, "store"))
+	if err != nil && !os.IsNotExist(err) {
+		t.Fatal(err)
+	}
+
+	_, _, err = ExecuteCmd(createAddOperatorCmd(), "--name", "O")
+	require.NoError(t, err)
+
+	require.FileExists(t, filepath.Join(ts.Dir, "store", "O", ".nsc"))
+	require.FileExists(t, filepath.Join(ts.Dir, "store", "O", "O.jwt"))
+}
+
+func TestAddOperatorCreateOrName(t *testing.T) {
+	ts := NewEmptyStore(t)
+	defer ts.Done(t)
+
+	_, err := os.Lstat(filepath.Join(ts.Dir, "store"))
+	if err != nil && !os.IsNotExist(err) {
+		t.Fatal(err)
+	}
+	_, _, err = ExecuteCmd(createAddOperatorCmd(), "--name", "O", "--import", ts.Dir)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "either name or import")
+}
+
+func TestImportOperator(t *testing.T) {
+	ts := NewEmptyStore(t)
+	defer ts.Done(t)
+
+	_, pub, kp := CreateOperatorKey(t)
+	oc := jwt.NewOperatorClaims(pub)
+	oc.Name = "O"
+	token, err := oc.Encode(kp)
+	require.NoError(t, err)
+	tf := filepath.Join(ts.Dir, "O.jwt")
+	err = Write(tf, []byte(token))
+	require.NoError(t, err)
+
+	_, _, err = ExecuteCmd(createAddOperatorCmd(), "--import", tf)
+	require.NoError(t, err)
+	storeFile := filepath.Join(ts.Dir, "store", "O", ".nsc")
+	require.FileExists(t, storeFile)
+
+	d, err := Read(storeFile)
+	var info store.Info
+	json.Unmarshal(d, &info)
+	require.True(t, info.Managed)
+	require.Equal(t, "O", info.EntityName)
+
+	target := filepath.Join(ts.Dir, "store", "O", "O.jwt")
+	require.FileExists(t, target)
+	d, err = Read(target)
+	require.NoError(t, err)
+	require.Equal(t, token, string(d))
+}
+
+func TestAddOperatorInteractive(t *testing.T) {
+	ts := NewEmptyStore(t)
+	defer ts.Done(t)
+
+	_, _, err := ExecuteInteractiveCmd(createAddOperatorCmd(), []interface{}{false, "O", "2019-12-01", "2029-12-01", true})
+	require.NoError(t, err)
+	d, err := Read(filepath.Join(ts.Dir, "store", "O", "O.jwt"))
+	oc, err := jwt.DecodeOperatorClaims(string(d))
+	require.Equal(t, oc.Name, "O")
+	start := time.Unix(oc.NotBefore, 0).UTC()
+	require.Equal(t, 2019, start.Year())
+	require.Equal(t, time.Month(12), start.Month())
+	require.Equal(t, 1, start.Day())
+
+	expiry := time.Unix(oc.Expires, 0).UTC()
+	require.Equal(t, 2029, expiry.Year())
+	require.Equal(t, time.Month(12), expiry.Month())
+	require.Equal(t, 1, expiry.Day())
+}
+
+func TestImportOperatorInteractive(t *testing.T) {
+	ts := NewEmptyStore(t)
+	defer ts.Done(t)
+
+	_, pub, kp := CreateOperatorKey(t)
+	oc := jwt.NewOperatorClaims(pub)
+	oc.Name = "O"
+	token, err := oc.Encode(kp)
+	require.NoError(t, err)
+	tf := filepath.Join(ts.Dir, "O.jwt")
+	err = Write(tf, []byte(token))
+	require.NoError(t, err)
+
+	_, _, err = ExecuteInteractiveCmd(createAddOperatorCmd(), []interface{}{true, tf})
+	require.NoError(t, err)
+
+	target := filepath.Join(ts.Dir, "store", "O", "O.jwt")
+	require.FileExists(t, target)
+}
