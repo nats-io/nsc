a1650cfa9caee09f9b681886a60e9c4a3c69af26
Stephen Asbury <sasbury@synadia.com>
2019-09-11 15:21:00 -0700

    Added random name to init if no name is set Added random name option to add user and add account using *

---
+++ cmd/names_generator_test.go
@@ -0,0 +1,28 @@
+// See names_generator.go for copyright
+package cmd
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestNameFormat(t *testing.T) {
+	name := GetRandomName(0)
+	if !strings.Contains(name, "_") {
+		t.Fatalf("Generated name does not contain an underscore")
+	}
+	if strings.ContainsAny(name, "0123456789") {
+		t.Fatalf("Generated name contains numbers!")
+	}
+}
+
+func TestNameRetries(t *testing.T) {
+	name := GetRandomName(1)
+	if !strings.Contains(name, "_") {
+		t.Fatalf("Generated name does not contain an underscore")
+	}
+	if !strings.ContainsAny(name, "0123456789") {
+		t.Fatalf("Generated name doesn't contain a number")
+	}
+
+}
