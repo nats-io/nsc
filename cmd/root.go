e77f75886a856099b841e1c940e212f8e300fc19
Shantanu Bansal <shantanubansal05@gmail.com>
2020-02-12 12:55:42 +0530

    Just adding one more useful function

--- cmd/root.go
+++ cmd/root.go
@@ -140,8 +140,8 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	cli.SetOutput(rootCmd.OutOrStderr())
-	if err := GetRootCmd().Execute(); err != nil {
+	err := ExecuteWithWriter(rootCmd.OutOrStderr())
+	if err != nil {
 		os.Exit(1)
 	}
 }
