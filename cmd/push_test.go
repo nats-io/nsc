d23e60d0d0c0f2710293dff457ec8fc4bad4fb97
Alberto Ricart <alberto@synadia.com>
2020-05-21 15:03:51 -0500

    Rename account (#274)
    * added internal command to rename an account

--- cmd/push_test.go
+++ cmd/push_test.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 The NATS Authors
+ * Copyright 2018-2020 The NATS Authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,10 +16,10 @@
 package cmd
 
 import (
-	"github.com/nats-io/jwt"
 	"runtime"
 	"testing"
 
+	"github.com/nats-io/jwt"
 	"github.com/stretchr/testify/require"
 )
 
