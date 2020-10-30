f08fb777214112aa28860537a911ec14ad8bd685
Matthias Hanel <mh@synadia.com>
2020-10-30 18:24:52 -0400

    [feat] Adding command to import account (#313)
    * [feat] Adding command to import account and user
    
    If the account is self signed, resign with the operator.
    User import takes jwt and creds file. Will import key as well then.
    
    Signed-off-by: Matthias Hanel <mh@synadia.com>
    
    * Incorporating comments
    
    Signed-off-by: Matthias Hanel <mh@synadia.com>

---
+++ cmd/importaccount.go
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2020-2020 The NATS Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/nats-io/jwt"
+	"github.com/nats-io/nkeys"
+	"github.com/nats-io/nsc/cmd/store"
+	"github.com/spf13/cobra"
+)
+
+func createImportAccountCmd() *cobra.Command {
+	var params ImportAccount
+	cmd := &cobra.Command{
+		Use:          "account --file <account-jwt>",
+		Short:        "Imports an account from a jwt file and resign with operator if self signed",
+		Example:      `nsc import account --file <account-jwt>`,
+		Args:         MaxArgs(0),
+		SilenceUsage: false,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := RunMaybeStorelessAction(cmd, args, &params); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+	cmd.Flags().BoolVarP(&params.skip, "skip", "", false, "skip validation issues, they can be edited out prior to push")
+	cmd.Flags().BoolVarP(&params.overwrite, "overwrite", "", false, "overwrite existing account")
+	cmd.Flags().StringVarP(&params.file, "file", "j", "", "account jwt to import")
+	return cmd
+}
+
+func init() {
+	importCmd.AddCommand(createImportAccountCmd())
+}
+
+type fileImport struct {
+	file      string
+	skip      bool
+	overwrite bool
+	content   []byte
+}
+
+func (p *fileImport) PreInteractive(ctx ActionCtx) (err error) {
+	return nil
+}
+
+func (p *fileImport) PostInteractive(ctx ActionCtx) (err error) {
+	return nil
+}
+
+func (p *fileImport) Load(ctx ActionCtx) (err error) {
+	p.content, err = ioutil.ReadFile(p.file)
+	return
+}
+
+func (p *fileImport) Validate(xtx ActionCtx, fileEndings ...string) error {
+	fi, err := os.Stat(p.file)
+	if err != nil {
+		return err
+	}
+	if fi.IsDir() {
+		return fmt.Errorf("%#q is a directory", p.file)
+	}
+	for _, ending := range fileEndings {
+		if strings.HasSuffix(p.file, ending) {
+			return nil
+		}
+	}
+	return fmt.Errorf("expected these file endings: %v", fileEndings)
+}
+
+type ImportAccount struct {
+	SignerParams
+	fileImport
+}
+
+func (p *ImportAccount) SetDefaults(ctx ActionCtx) error {
+	p.SignerParams.SetDefaults(nkeys.PrefixByteOperator, true, ctx)
+	return nil
+}
+
+func (p *ImportAccount) Validate(ctx ActionCtx) error {
+	if err := p.fileImport.Validate(ctx, ".jwt"); err != nil {
+		return err
+	}
+	if err := p.SignerParams.Resolve(ctx); err != nil {
+		return err
+	}
+	if !ctx.StoreCtx().Store.IsManaged() {
+		signers, err := GetOperatorSigners(ctx)
+		if err != nil {
+			return err
+		}
+		ok, err := ValidSigner(p.SignerParams.signerKP, signers)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return errors.New("invalid account signer")
+		}
+	}
+
+	return nil
+}
+
+func (p *ImportAccount) Run(ctx ActionCtx) (store.Status, error) {
+	r := store.NewDetailedReport(true)
+	theJWT := p.content
+	claim, err := jwt.DecodeAccountClaims(string(theJWT))
+	if err != nil {
+		r.AddError("failed to decode %#q: %v", p.file, err)
+		return r, err
+	}
+	if validateAndReport(claim, p.skip, r) {
+		return r, nil
+	}
+	if ctx.StoreCtx().Store.HasAccount(claim.Name) {
+		if !p.overwrite {
+			r.AddError("account already exists, overwrite with --overwrite")
+			return r, nil
+		}
+		if old, err := ctx.StoreCtx().Store.ReadAccountClaim(claim.Name); err != nil {
+			r.AddError("existing Account not found: %v", err)
+			return r, nil
+		} else if old.Subject != claim.Subject {
+			r.AddError("existing Account has a name collision only and does not reference the same entity "+
+				"(Subject is %s and differs from %s). This problem needs to be resolved manually.",
+				old.Subject, claim.Subject)
+			return r, nil
+		}
+	}
+	sameOperator := false
+	keys, _ := ctx.StoreCtx().GetOperatorKeys()
+	for _, key := range keys {
+		if key == claim.Issuer {
+			sameOperator = true
+			break
+		}
+	}
+	if ctx.StoreCtx().Store.IsManaged() {
+		if claim.IsSelfSigned() {
+			r.AddError("only a non managed store can import a self signed account")
+			return r, nil
+		}
+	} else if claim.IsSelfSigned() {
+		if jwt, err := claim.Encode(p.signerKP); err != nil {
+			r.AddError("error during encoding of self signed account jwt: %v", err)
+			return r, nil
+		} else {
+			theJWT = []byte(jwt)
+			sameOperator = true
+		}
+	}
+	if !sameOperator {
+		r.AddError("can only import account signed by same operator. Possibly update your operator")
+		return r, nil
+	}
+	sub, err := ctx.StoreCtx().Store.StoreClaim(theJWT)
+	if err != nil {
+		r.AddError("error when storing account: %v", err)
+	}
+	r.Add(sub)
+	if r.HasNoErrors() {
+		r.AddOK("account %s was successfully imported", claim.Name)
+	} else {
+		r.AddOK("account %s was not imported: %v", claim.Name, err)
+	}
+	return r, nil
+}
---
+++ cmd/importaccount_test.go
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020-2020 The NATS Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/nats-io/jwt"
+	"github.com/nats-io/nkeys"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ImportAccountSelfSigned(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	akp, _ := nkeys.CreateAccount()
+	pk, _ := akp.PublicKey()
+	ac := jwt.NewAccountClaims(pk)
+	ac.Name = ac.Subject
+	theJWT, err := ac.Encode(akp)
+	require.NoError(t, err)
+	require.True(t, ac.IsSelfSigned())
+
+	check := func() {
+		t.Helper()
+		claim, err := ts.Store.ReadAccountClaim(pk)
+		require.NoError(t, err)
+		require.False(t, claim.IsSelfSigned())
+	}
+
+	file := filepath.Join(ts.Dir, "account-selfsigned.jwt")
+	err = ioutil.WriteFile(file, []byte(theJWT), 0666)
+	require.NoError(t, err)
+	_, _, err = ExecuteCmd(createImportAccountCmd(), "--file", file)
+	require.NoError(t, err)
+	check()
+	_, _, err = ExecuteCmd(createImportAccountCmd(), "--file", file)
+	require.Error(t, err)
+	_, _, err = ExecuteCmd(createImportAccountCmd(), "--file", file, "--overwrite")
+	require.NoError(t, err)
+	check()
+}
+
+func Test_ImportAccountOtherOperator(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+	oKp, _ := nkeys.CreateOperator()
+	akp, _ := nkeys.CreateAccount()
+	pk, _ := akp.PublicKey()
+	ac := jwt.NewAccountClaims(pk)
+	ac.Name = ac.Subject
+	theJWT, err := ac.Encode(oKp)
+	require.NoError(t, err)
+	require.False(t, ac.IsSelfSigned())
+	file := filepath.Join(ts.Dir, "account.jwt")
+	err = ioutil.WriteFile(file, []byte(theJWT), 0666)
+	require.NoError(t, err)
+	_, _, err = ExecuteCmd(createImportAccountCmd(), "--file", file)
+	require.Error(t, err)
+}
