b38d1b85f43c5b65d05703d67d1ebf9ad698c4d6
Scott Fauerbach <scottfauerbach@gmail.com>
2022-02-07 19:33:31 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -11,68 +11,168 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#Requires -RunAsAdministrator
+
 $ErrorActionPreference = 'Stop'
+$CurrentNightlyUrl = "https://get-nats.io/current-nightly"
+$PlatformsUrl = "https://get-nats.io/synadia-nats-platforms.json"
+$NATSDir = "NATS"
+$OSInfo = "windows-amd64"
+$Stable = "stable"
+$Nightly = "nightly"
+$NscTool = "nsc"
+$NscExe = "nsc.exe"
+$NscZip = "nsc.zip"
 
-$Version = ""
-if ($args.Length -eq 1) {
-    $Version = $args.Get(0)
+# ----------------------------------------------------------------------------------------------------
+# Functions
+# ----------------------------------------------------------------------------------------------------
+Function Read-NightlyVersion() {
+	if (!$_currentNightly) {
+		$temp = [string](Invoke-WebRequest -Uri $CurrentNightlyUrl)
+		$_currentNightly = $temp.Split("`r?`n")[0]
+	}
+	return $_currentNightly
 }
 
-$NscInstall = $env:NSC_INSTALL
-$BinDir = if ($NscInstall) {
-    "$NscInstall\bin"
-} else {
-    "$Home\.nats\bin"
+# Example Url for Read From Url "https://github.com/nats-io/natscli/releases/download/v0.0.28/nats-0.0.28-windows-386.zip"
+Function Read-VersionFromUrl($Url) {
+	$at = $Url.Indexof("download/")
+	$temp = $Url.Substring($at + 9)
+	$at = $temp.Indexof("/")
+	$temp = $temp.Substring(0, $at);
+	if ($temp.StartsWith("v")) {
+		return $temp
+	}
+	return "v$temp"
 }
 
-$NscZip = "$BinDir\nsc.zip"
-$NscExe = "$BinDir\nsc.exe"
-$Target = 'windows-amd64'
-
-if (Test-Path "$BinDir\nsc.exe") {
-	Write-Host "NSC binary ($NscExe) already exists, exiting script.`r`n"
-	Exit -1
+Function Read-ArchiveFolderNameFromUrl($Url) {
+	$at = $Url.LastIndexOf("/")
+	$temp = $Url.Substring($at + 1)
+	$at = $temp.LastIndexOf(".zip")
+	return $temp.Substring(0, $at);
 }
 
-if ( !(Test-Path $BinDir) ) {
-    New-Item $BinDir -ItemType Directory | Out-Null
-	Write-Host "NSC directory ($BinDir) created...`r`n"
+Function Format-EndWithBackslash($s) {
+	if ($s.EndsWith("\")){
+		return $s
+	}
+	return $s + "\"
 }
 
-# Download the archive with the exe from github
-## GitHub requires TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-## Uri is different when a version is requested
-if (!$Version) {
-    Write-Host "Downloading latest nsc...`r`n"
-    $NscUri = "https://github.com/nats-io/nsc/releases/latest/download/nsc-${Target}.zip"
-} else {
-    Write-Host "Downloading nsc version ${Version}...`r`n"
-    $NscUri = "https://github.com/nats-io/nsc/releases/download/${Version}/nsc-${Target}.zip"
+Function Format-DoesntEndWithBackslash($s) {
+	if ($s.EndsWith("\")){
+		return $s.Substring(0, $s.Length - 1)
+	}
+	return $s
 }
 
-## Actual download
-Invoke-WebRequest -Uri $NscUri -OutFile $NscZip -UseBasicParsing
+Function Select-Folder() {
+	[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+	$FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+	$FolderBrowserDialog.RootFolder = 'MyComputer'
+	[void] $FolderBrowserDialog.ShowDialog()
+	return $FolderBrowserDialog.SelectedPath
+}
 
-## Unzip
-Write-Host "Extracting nsc.exe...`r`n"
-Expand-Archive -Path $NscZip -DestinationPath $BinDir
+Function Invoke-Backup($BinDir, $Tool, $ExePath) {
+	if ( Test-Path $ExePath )
+	{
+		Write-Host "Backing up existing $Tool executable..."
+		for($i = 1; $i -lt 100; $i++) # I give up after 99 tries
+		{
+			$nn = "$BinDir$Tool-" + (Get-Date -Format "yyyyMMdd") + "-backup$i.exe"
+			if (!(Test-Path $nn))
+			{
+				Rename-Item -Path $ExePath -NewName $nn
+				return
+			}
+		}
+		Write-Host "Cannot backup $ExePath, all backups exist."
+		Exit -2
+	}
+}
 
-# Remove downloaded archive file
-Write-Host "Removing downloaded archive...`r`n"
-Remove-Item $NscZip
+# ----------------------------------------------------------------------------------------------------
+# Execution
+# ----------------------------------------------------------------------------------------------------
+# They get to pick the folder given a default
+$tempDir = (Format-EndWithBackslash $Env:ProgramFiles) + $NATSDir
+$opt0 = New-Object System.Management.Automation.Host.ChoiceDescription "&Default Location","Default Location is $tempDir"
+$opt1 = New-Object System.Management.Automation.Host.ChoiceDescription "&Choose Location","Choose your location."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($opt0, $opt1)
+$result = $host.ui.PromptForChoice("Installation Location", "Where will the programs be installed? Default Location is $tempDir", $options, 0)
+if ($result -eq 1) {
+	$tempDir = Select-Folder
+	if (!$tempDir) {
+		Write-Host "You must pick a directory. Exiting"
+		Exit -1
+	}
+}
+$binDir = Format-EndWithBackslash $tempDir
+$binDirNoSlash = Format-DoesntEndWithBackslash $binDir
+if ( !(Test-Path $binDirNoSlash) ) {
+	New-Item $binDirNoSlash -ItemType Directory | Out-Null
+}
+
+# some local variables now that I have $binDir
+$nscExePath = $binDir + $NscExe
+$nscZipLocal =  $binDir + $NscZip
+
+# $channel Have the user pick which type of channel they want, i.e. stable or nightly. Get the channel from the conf
+$dataDir = (Format-EndWithBackslash $Env:LOCALAPPDATA) + $NATSDir
+$dataFile = "$dataDir\install-channel.txt"
+$dflt = 0
+if ( Test-Path $dataFile ) {
+	$chn = (Get-Content -Path $dataFile).Split("`r?`n")[0]
+	if ($chn -eq $Nightly) {
+		$dflt = 1
+	}
+}
+$opt0 = New-Object System.Management.Automation.Host.ChoiceDescription "&$Stable","Latest Stable Build."
+$opt1 = New-Object System.Management.Automation.Host.ChoiceDescription "&$Nightly","Current Nightly Build."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($opt0, $opt1)
+$result = $host.ui.PromptForChoice("$Channel Selection", "Which channel do you want to install?", $options, $dflt)
+switch ($result) {
+	0{$channel = $Stable}
+	1{$channel = $Nightly}
+}
+Write-Host ""
 
 # Add bin dir to path if not already in path
-Write-Host "Updating $BinDir for the User's path...`r`n"
-$User = [EnvironmentVariableTarget]::User
-$Path = [Environment]::GetEnvironmentVariable('Path', $User)
-if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) {
-    [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
-    $Env:Path += ";$BinDir"
+Write-Host "Ensuring $binDirNoSlash is in the path..."
+$Machine = [EnvironmentVariableTarget]::Machine
+$Path = [Environment]::GetEnvironmentVariable('Path', $Machine)
+if (!(";$Path;".ToLower() -like "*;$binDirNoSlash;*".ToLower())) {
+	[Environment]::SetEnvironmentVariable('Path', "$Path;$binDirNoSlash", $Machine)
+	$Env:Path += ";$binDirNoSlash"
 }
 
-Write-Host "NSC was installed successfully to $NscExe. Running 'nsc --version' to verify...`r`n"
-nsc --version
+Write-Host "Downloading platform info..."
+$json = (Invoke-WebRequest https://get-nats.io/synadia-nats-platforms.json -ContentType "application/json" -UseBasicParsing) | ConvertFrom-Json
+$nscZipUrl = $json.$channel.platforms.$OSInfo.tools.$NscTool.zip_url
 
-Write-Host "`r`nRun 'nsc --help' to get started.`r`n"
+if ($channel -eq $Nightly) {
+	$verNsc = $verNats = Read-NightlyVersion
+	Write-Host "$NscTool $Nightly version $verNsc"
+	$nscZipUrl = $nscZipUrl.Replace("%NIGHTLY%", $verNsc)
+}
+else {
+	$verNsc = Read-VersionFromUrl $nscZipUrl
+	Write-Host "$NscTool $Stable version $verNsc"
+}
+
+# Download the zip files
+Write-Host "Downloading archive $nscZipUrl..."
+Invoke-WebRequest -Uri $nscZipUrl -OutFile $nscZipLocal -UseBasicParsing
+
+# Backup existing versions now that the downloads worked
+Invoke-Backup $binDir $NscTool $nscExePath
+
+# nsc: Unzip, Remove Archive
+Write-Host "Installing $NscTool..."
+Expand-Archive -Path $nscZipLocal -DestinationPath $binDir
+Remove-Item $nscZipLocal
+
+Write-Host "Done!`r`n"
