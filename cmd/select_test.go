d5cdf93ec3c5d61bd80ee7e895e5741b44b85d58
Seth Back <sethjback@gmail.com>
2023-01-25 09:03:53 -0800

    use testify's require instead of assert

--- cmd/select_test.go
+++ cmd/select_test.go
@@ -18,65 +18,63 @@ package cmd
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSelectAccount(t *testing.T) {
-	assert := assert.New(t)
+	require := require.New(t)
 	ts := NewTestStore(t, "test")
 	defer ts.Done(t)
 
 	_, _, err := ExecuteCmd(selectAccountCmd(), "A")
-	assert.NotNil(err)
+	require.NotNil(err)
 
 	ts.AddAccount(t, "A")
 	ts.AddAccount(t, "B")
 
 	_, _, err = ExecuteCmd(selectAccountCmd(), "A")
-	assert.Nil(err)
+	require.Nil(err)
 	conf := GetConfig()
-	assert.Equal(conf.Account, "A")
+	require.Equal(conf.Account, "A")
 
 	_, _, err = ExecuteCmd(selectAccountCmd(), "B")
-	assert.Nil(err)
+	require.Nil(err)
 	conf = GetConfig()
-	assert.Equal(conf.Account, "B")
+	require.Equal(conf.Account, "B")
 
 	_, _, err = ExecuteCmd(selectAccountCmd(), "NO")
-	if assert.NotNil(err) {
-		assert.Contains(err.Error(), "\"NO\" not in accounts for operator")
-	}
+	require.NotNil(err)
+	require.Contains(err.Error(), "\"NO\" not in accounts for operator")
 
 	conf = GetConfig()
-	assert.Equal(conf.Account, "B")
+	require.Equal(conf.Account, "B")
 }
 
 func TestSelectOperator(t *testing.T) {
-	assert := assert.New(t)
+	require := require.New(t)
 	ts := NewTestStore(t, "test")
 	defer ts.Done(t)
 
 	_, _, err := ExecuteCmd(selectOperatorCmd(), "test")
-	assert.Nil(err)
+	require.Nil(err)
 
 	ts.AddOperator(t, "test2")
 	ts.AddOperator(t, "test3")
 
 	_, _, err = ExecuteCmd(selectOperatorCmd(), "test2")
-	assert.Nil(err)
+	require.Nil(err)
 	conf := GetConfig()
-	assert.Equal(conf.Operator, "test2")
+	require.Equal(conf.Operator, "test2")
 
 	_, _, err = ExecuteCmd(selectOperatorCmd(), "test3")
-	assert.Nil(err)
+	require.Nil(err)
 	conf = GetConfig()
-	assert.Equal(conf.Operator, "test3")
+	require.Equal(conf.Operator, "test3")
 
 	_, _, err = ExecuteCmd(selectOperatorCmd(), "NO")
-	if assert.NotNil(err) {
-		assert.Contains(err.Error(), "operator \"NO\" not in")
-	}
+	require.NotNil(err)
+	require.Contains(err.Error(), "operator \"NO\" not in")
 
 	conf = GetConfig()
-	assert.Equal(conf.Operator, "test3")
+	require.Equal(conf.Operator, "test3")
 }
