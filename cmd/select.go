f437254a10067ba3b2527cabfc42d1a392745f0c
Seth Back <sethjback@gmail.com>
2023-01-25 07:34:56 -0800

    add select command

---
+++ cmd/select.go
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2018-2022 The NATS Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	GetRootCmd().AddCommand(createSelecteCmd())
+}
+
+func createSelecteCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "select",
+		Short: "Set the current operator or account",
+	}
+	cmd.AddCommand(selectOperatorCmd())
+	cmd.AddCommand(selectAccountCmd())
+	return cmd
+}
+
+func selectOperatorCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "operator",
+		Short: "set the operator",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if err := cobra.ExactArgs(1)(cmd, args); err != nil {
+				return err
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if NscCwdOnly {
+				return fmt.Errorf("%s is set - change your cwd to use context", NscCwdOnlyEnv)
+			}
+
+			current := GetConfig()
+			if err := current.ContextConfig.Update("", args[0], ""); err != nil {
+				return err
+			}
+
+			return current.Save()
+		},
+	}
+}
+
+func selectAccountCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "account",
+		Short: "set the account",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if err := cobra.ExactArgs(1)(cmd, args); err != nil {
+				return err
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if NscCwdOnly {
+				return fmt.Errorf("%s is set - change your cwd to use context", NscCwdOnlyEnv)
+			}
+
+			current := GetConfig()
+			if err := current.ContextConfig.Update("", "", args[0]); err != nil {
+				return err
+			}
+
+			return current.Save()
+		},
+	}
+}
---
+++ cmd/select_test.go
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2018-2022 The NATS Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSelectAccount(t *testing.T) {
+	assert := assert.New(t)
+	ts := NewTestStore(t, "test")
+	defer ts.Done(t)
+
+	_, _, err := ExecuteCmd(selectAccountCmd(), "A")
+	assert.NotNil(err)
+
+	ts.AddAccount(t, "A")
+	ts.AddAccount(t, "B")
+
+	_, _, err = ExecuteCmd(selectAccountCmd(), "A")
+	assert.Nil(err)
+	conf := GetConfig()
+	assert.Equal(conf.Account, "A")
+
+	_, _, err = ExecuteCmd(selectAccountCmd(), "B")
+	assert.Nil(err)
+	conf = GetConfig()
+	assert.Equal(conf.Account, "B")
+
+	_, _, err = ExecuteCmd(selectAccountCmd(), "NO")
+	if assert.NotNil(err) {
+		assert.Contains(err.Error(), "\"NO\" not in accounts for operator")
+	}
+
+	conf = GetConfig()
+	assert.Equal(conf.Account, "B")
+}
+
+func TestSelectOperator(t *testing.T) {
+	assert := assert.New(t)
+	ts := NewTestStore(t, "test")
+	defer ts.Done(t)
+
+	_, _, err := ExecuteCmd(selectOperatorCmd(), "test")
+	assert.Nil(err)
+
+	ts.AddOperator(t, "test2")
+	ts.AddOperator(t, "test3")
+
+	_, _, err = ExecuteCmd(selectOperatorCmd(), "test2")
+	assert.Nil(err)
+	conf := GetConfig()
+	assert.Equal(conf.Operator, "test2")
+
+	_, _, err = ExecuteCmd(selectOperatorCmd(), "test3")
+	assert.Nil(err)
+	conf = GetConfig()
+	assert.Equal(conf.Operator, "test3")
+
+	_, _, err = ExecuteCmd(selectOperatorCmd(), "NO")
+	if assert.NotNil(err) {
+		assert.Contains(err.Error(), "operator \"NO\" not in")
+	}
+
+	conf = GetConfig()
+	assert.Equal(conf.Operator, "test3")
+}
