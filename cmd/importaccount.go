5e583722eca4381ff07d01ddcccf784eb140a7b5
Alberto Ricart <alberto@synadia.com>
2023-04-17 19:33:38 -0500

    [FIX] added checks the store API for situations where the returned store by a context is nil, this prevents panics when trying to resolve entities that cannot be referenced due to the lack of context. (#583)
    FIXES #578

--- cmd/importaccount.go
+++ cmd/importaccount.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2022 The NATS Authors
+ * Copyright 2018-2023 The NATS Authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -83,7 +83,11 @@ func (p *fileImport) Load(ctx ActionCtx) (err error) {
 }
 
 func (p *fileImport) Validate(xtx ActionCtx, fileEndings ...string) error {
-	fi, err := os.Stat(p.file)
+	fp, err := Expand(p.file)
+	if err != nil {
+		return err
+	}
+	fi, err := os.Stat(fp)
 	if err != nil {
 		return err
 	}
