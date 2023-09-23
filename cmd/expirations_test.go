38759f5a1a61972d575248ab486d8157c3a22862
Alberto Ricart <alberto@synadia.com>
2023-09-23 09:49:00 -0500

    [FEAT] added expirations command which traverses the current operator checking for JWTs and creds that expire within a specified amount of time (by default a month). The command can generate a report in JSON format that could be used by other tooling.

---
+++ cmd/expirations_test.go
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2018-2023 The NATS Authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func Test_ExpirationsNone(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	ts.AddAccount(t, "A")
+	ts.AddUser(t, "A", "U")
+
+	stdout, _, err := ExecuteCmd(createExpirationsCommand(), "--json")
+	require.NoError(t, err)
+
+	var expirations ExpirationReportJSON
+	err = json.Unmarshal([]byte(stdout), &expirations)
+	require.NoError(t, err)
+	_, err = time.Parse(time.RFC3339, expirations.ExpirationThreshold)
+	require.NoError(t, err)
+	require.Len(t, expirations.Report, 4)
+}
+
+func Test_ExpirationsOperator(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	oc, err := ts.Store.ReadOperatorClaim()
+	require.NoError(t, err)
+	// an hour ago
+	oc.Expires = time.Now().Add(-time.Hour).UTC().Unix()
+	token, err := oc.Encode(ts.OperatorKey)
+	require.NoError(t, err)
+	err = ts.Store.StoreRaw([]byte(token))
+	require.NoError(t, err)
+
+	stdout, _, err := ExecuteCmd(createExpirationsCommand(), "--json")
+	require.NoError(t, err)
+
+	var expirations ExpirationReportJSON
+	err = json.Unmarshal([]byte(stdout), &expirations)
+	require.NoError(t, err)
+	require.Len(t, expirations.Report, 1)
+	require.True(t, expirations.Report[0].Expired)
+}
+
+func Test_ExpirationAccount(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	ts.AddAccount(t, "A")
+	ac, err := ts.Store.ReadAccountClaim("A")
+	require.NoError(t, err)
+	ac.Expires = time.Now().Add(-time.Minute).UTC().Unix()
+	token, err := ac.Encode(ts.GetAccountKey(t, "A"))
+	require.NoError(t, err)
+	err = ts.Store.StoreRaw([]byte(token))
+	require.NoError(t, err)
+
+	stdout, _, err := ExecuteCmd(createExpirationsCommand(), "--json")
+	require.NoError(t, err)
+
+	var expirations ExpirationReportJSON
+	err = json.Unmarshal([]byte(stdout), &expirations)
+	require.NoError(t, err)
+	require.Len(t, expirations.Report, 2)
+	require.False(t, expirations.Report[0].Expired)
+	require.True(t, expirations.Report[1].Expired)
+}
+
+func Test_ExpirationUser(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	ts.AddAccount(t, "A")
+	ts.AddUser(t, "A", "U")
+	uc, err := ts.Store.ReadUserClaim("A", "U")
+	require.NoError(t, err)
+	uc.Expires = time.Now().Add(-time.Minute).UTC().Unix()
+	token, err := uc.Encode(ts.GetAccountKey(t, "A"))
+	require.NoError(t, err)
+	err = ts.Store.StoreRaw([]byte(token))
+	require.NoError(t, err)
+
+	stdout, _, err := ExecuteCmd(createExpirationsCommand(), "--json")
+	require.NoError(t, err)
+
+	var expirations ExpirationReportJSON
+	err = json.Unmarshal([]byte(stdout), &expirations)
+	require.NoError(t, err)
+	require.Len(t, expirations.Report, 4)
+	require.False(t, expirations.Report[0].Expired)
+	require.False(t, expirations.Report[1].Expired)
+	require.True(t, expirations.Report[2].Expired)
+	// we didn't update the creds
+	require.False(t, expirations.Report[3].Expired)
+}
+
+func Test_ExpiresSoonOperator(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	oc, err := ts.Store.ReadOperatorClaim()
+	require.NoError(t, err)
+	// an hour ago
+	oc.Expires = time.Now().Add(time.Hour * 24 * 7).UTC().Unix()
+	token, err := oc.Encode(ts.OperatorKey)
+	require.NoError(t, err)
+	err = ts.Store.StoreRaw([]byte(token))
+	require.NoError(t, err)
+
+	stdout, _, err := ExecuteCmd(createExpirationsCommand(), "--json")
+	require.NoError(t, err)
+
+	var expirations ExpirationReportJSON
+	err = json.Unmarshal([]byte(stdout), &expirations)
+	require.NoError(t, err)
+	require.Len(t, expirations.Report, 1)
+	require.False(t, expirations.Report[0].Expired)
+	require.True(t, expirations.Report[0].ExpiresSoon)
+}
+
+func Test_ExpiresSoonAccount(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	ts.AddAccount(t, "A")
+	ac, err := ts.Store.ReadAccountClaim("A")
+	require.NoError(t, err)
+	ac.Expires = time.Now().Add(time.Minute * 2).UTC().Unix()
+	token, err := ac.Encode(ts.GetAccountKey(t, "A"))
+	require.NoError(t, err)
+	err = ts.Store.StoreRaw([]byte(token))
+	require.NoError(t, err)
+
+	stdout, _, err := ExecuteCmd(createExpirationsCommand(), "--json")
+	require.NoError(t, err)
+
+	var expirations ExpirationReportJSON
+	err = json.Unmarshal([]byte(stdout), &expirations)
+	require.NoError(t, err)
+	require.Len(t, expirations.Report, 2)
+	require.False(t, expirations.Report[0].Expired)
+	require.False(t, expirations.Report[1].Expired)
+	require.True(t, expirations.Report[1].ExpiresSoon)
+}
+
+func Test_ExpiresSoonUser(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	ts.AddAccount(t, "A")
+	ts.AddUser(t, "A", "U")
+	uc, err := ts.Store.ReadUserClaim("A", "U")
+	require.NoError(t, err)
+	uc.Expires = time.Now().Add(time.Hour).UTC().Unix()
+	token, err := uc.Encode(ts.GetAccountKey(t, "A"))
+	require.NoError(t, err)
+	err = ts.Store.StoreRaw([]byte(token))
+	require.NoError(t, err)
+
+	creds, err := GenerateConfig(ts.Store, "A", "U", ts.GetUserKey(t, "A", "U"))
+	require.NoError(t, err)
+	_, err = ts.KeyStore.MaybeStoreUserCreds("A", "U", creds)
+	require.NoError(t, err)
+
+	stdout, _, err := ExecuteCmd(createExpirationsCommand(), "--json")
+	require.NoError(t, err)
+
+	var expirations ExpirationReportJSON
+	err = json.Unmarshal([]byte(stdout), &expirations)
+	require.NoError(t, err)
+	require.Len(t, expirations.Report, 4)
+	require.False(t, expirations.Report[0].Expired)
+	require.False(t, expirations.Report[1].Expired)
+	require.False(t, expirations.Report[2].Expired)
+	require.True(t, expirations.Report[2].ExpiresSoon)
+	require.False(t, expirations.Report[3].Expired)
+	require.True(t, expirations.Report[3].ExpiresSoon)
+	require.False(t, expirations.Report[3].Expired)
+
+	stdout, _, err = ExecuteCmd(createExpirationsCommand(), "--skip", "--json")
+	require.NoError(t, err)
+	err = json.Unmarshal([]byte(stdout), &expirations)
+	require.NoError(t, err)
+	require.Len(t, expirations.Report, 2)
+	require.Equal(t, expirations.Report[0].ID, uc.Subject)
+	require.True(t, expirations.Report[0].ExpiresSoon)
+	require.False(t, expirations.Report[0].Expired)
+	require.Equal(t, expirations.Report[1].ID, uc.Subject)
+	require.True(t, expirations.Report[1].ExpiresSoon)
+	require.False(t, expirations.Report[1].Expired)
+}
+
+func Test_ExpirationsTable(t *testing.T) {
+	ts := NewTestStore(t, "O")
+	defer ts.Done(t)
+
+	ts.AddAccount(t, "A")
+	ts.AddUser(t, "A", "U")
+	uc, err := ts.Store.ReadUserClaim("A", "U")
+	require.NoError(t, err)
+	uc.Expires = time.Now().Add(time.Hour).UTC().Unix()
+	token, err := uc.Encode(ts.GetAccountKey(t, "A"))
+	require.NoError(t, err)
+	err = ts.Store.StoreRaw([]byte(token))
+	require.NoError(t, err)
+
+	_, stderr, err := ExecuteCmd(createExpirationsCommand())
+	require.NoError(t, err)
+
+	require.Contains(t, stderr, "| O")
+	require.Contains(t, stderr, "| O/A")
+	require.Contains(t, stderr, "| Soon    | O/A/U")
+	require.Contains(t, stderr, "In 59 Minutes |")
+	require.Contains(t, stderr, "creds/O/A/U.creds")
+}
